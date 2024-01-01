7b3d7c05116fb006a49c852299988aee4da82f6b
Tadeu Pinheiro <tadeujr2014@gmail.com>
2024-01-01 20:40:38 -0300

    Criando o Component manual e com o Angular
    O component meu-primeiro foi criado manualmente, o app-meu-primeiro2 foi criado através do terminal. Não tem mais o arquivo module, então a configuração fica no app.component.ts no 'imports' abaixo da anotação @Component.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
+import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, MeuPrimeiroComponent, MeuPrimeiro2Component],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'primeiro-projeto';
+  title = 'Graziele';
 }
---
+++ src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-meu-primeiro2',
+  standalone: true,
+  imports: [],
+  templateUrl: './meu-primeiro2.component.html',
+  styleUrl: './meu-primeiro2.component.css'
+})
+export class MeuPrimeiro2Component {
+
+}
