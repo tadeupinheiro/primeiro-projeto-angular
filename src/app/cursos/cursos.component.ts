faaf9aa57dd8aa72675cd00b91d893ac4c4c78cb
Tadeu Pinheiro <tadeujr2014@gmail.com>
2024-01-03 13:25:55 -0300

    Templates
    Essas modificações foram durante aprendizado sobre templates, literal e url. Também entendi o conceito de transpolação.

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
   styleUrl: './cursos.component.css'
 })
-export class CursosComponent {
+export class CursosComponent implements OnInit {
+
+  nomePortal: string;
+  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+
+  constructor(){
+    this.nomePortal = 'http://loiane.training';
+
+  }
+  ngOnInit(){
+
+  }
 
 }
