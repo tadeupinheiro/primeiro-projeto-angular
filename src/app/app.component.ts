1f787c6c8753bf7f16526d1c913e38623c40b776
Tadeu Pinheiro <tadeujr2014@gmail.com>
2024-01-02 21:48:23 -0300

    Aprendendo sobre os modules
    Na aula o projeto criado pelo Angular cli vinha com um arquivo app.module, no angular cli atual não vem, é declarado dentro do app.component. Nesse commit excluí também o router outlet, o app configs e excluí as referências a esses arquivos dos arquivos main e main.server. Como não tinha nas aulas e ainda não sei a utilidade, achei melhor excluir. Aprendi que é interessante criar os modules para cada diretório, para controlar os components dentro dele e controlar o que cada module pode ou não ver. OBS: Precisei excluir os campos standalone e imports de cada component criado automaticamente com o Angular cli, pois ficava dando erro ao tentar importar no module.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,16 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterOutlet } from '@angular/router';
+
 import { MeuPrimeiroComponent } from './meu-primeiro/meu-primeiro.component';
 import { MeuPrimeiro2Component } from './meu-primeiro2/meu-primeiro2.component'
+import { CursosModule } from "./cursos/cursos.module";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet, MeuPrimeiroComponent, MeuPrimeiro2Component],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [CommonModule, MeuPrimeiroComponent, MeuPrimeiro2Component, CursosModule]
 })
 export class AppComponent {
   title = 'Graziele';
