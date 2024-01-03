7455dde8f0e5a62d8bf172ab07eadf4657aad495
Tadeu Pinheiro <tadeujr2014@gmail.com>
2024-01-03 13:55:11 -0300

    Injeção de dependências
    Aprendendo duas formas de injeção de dependencias. A forma automática que já vem no Angular, que é o providersIn dentro da própria anotação, e colocar providers no arquivo module do diretório da classe.

--- src/app/cursos/cursos.component.ts
+++ src/app/cursos/cursos.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-import { OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CursosService } from './cursos.service';
 
 @Component({
   selector: 'app-cursos',
@@ -9,11 +10,18 @@ import { OnInit } from '@angular/core';
 export class CursosComponent implements OnInit {
 
   nomePortal: string;
-  cursos: string[] = ['Java', 'Ext JS', 'Angular'];
+  cursos: string[];
 
-  constructor(){
+  constructor(private cursosService: CursosService){
+    /*Esse parâmetro no construtor, é pra facilitar o código, é o mesmo que declarrar o atributo da classe e depois colocar a variável passada
+    dentro do construtor recebendo a variável passada no ArgumentOutOfRangeError.
+    */
     this.nomePortal = 'http://loiane.training';
 
+    //var servico = new CursosService();
+    //Ao invés de instanciar a classe, a gente usa a injeção de dependencias.
+
+    this.cursos = this.cursosService.getCursos();
   }
   ngOnInit(){
 
